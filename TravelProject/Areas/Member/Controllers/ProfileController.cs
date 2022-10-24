2eb279cc59eaf520b941137b904dd72ff7ddcfef
ismailunlu <smlnl@outlook.com>
2022-10-25 00:54:12 +0300

    Added image insertion area

--- TravelProject/Areas/Member/Controllers/ProfileController.cs
+++ TravelProject/Areas/Member/Controllers/ProfileController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Travel.Entity.Concrete;
@@ -33,6 +34,33 @@ namespace TravelProject.MVC.Areas.Member.Controllers
             userEditViewModel.Mail = values.Email;
 
             return View(userEditViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel userEditViewModel)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (userEditViewModel != null)
+            {
+                var resource = Directory.GetCurrentDirectory();
+                var extension = Path.GetExtension(userEditViewModel.Image.FileName);
+                var imageName = Guid.NewGuid() + extension;
+                var saveLacoation = resource + "/wwwroot/UserImages" + imageName;
+                var stream = new FileStream(saveLacoation, FileMode.Create);
+                await userEditViewModel.Image.CopyToAsync(stream);
+                user.ImageUrl = imageName;
+
+            }
+            user.Name = userEditViewModel.Name;
+            user.Surname = userEditViewModel.SurName;
+            user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, userEditViewModel.Password);
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("SıgnIn", "Login");
+            }
+
+            return View();
         }
     }
 }
--- TravelProject/Areas/Member/Models/UserEditViewModel.cs
+++ TravelProject/Areas/Member/Models/UserEditViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,10 +10,11 @@ namespace TravelProject.MVC.Areas.Member.Models
     {
         public string Name { get; set; }
         public string SurName { get; set; }
-        public int Password { get; set; }
-        public int ConfirmPassword { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
         public string PhoneNumber { get; set; }
         public string Mail { get; set; }
         public string ImageUrl { get; set; }
+        public IFormFile Image { get; set; }
     }
 }
